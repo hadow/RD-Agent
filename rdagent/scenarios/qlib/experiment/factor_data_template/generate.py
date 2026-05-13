23c87ae51120a200fb53cb07d5cbdb9563a74839
DD Hao <hadowhadow@gmail.com>
2026-05-13 20:54:47 +0800

    Update generate.py

--- rdagent/scenarios/qlib/experiment/factor_data_template/generate.py
+++ rdagent/scenarios/qlib/experiment/factor_data_template/generate.py
@@ -6,22 +6,39 @@ from qlib.data import D
 
 instruments = D.instruments()
 fields = ["$open", "$close", "$high", "$low", "$volume", "$factor"]
-data = D.features(instruments, fields, freq="day").swaplevel().sort_index().loc["2008-12-29":].sort_index()
 
-data.to_hdf("./daily_pv_all.h5", key="data")
-
-
-fields = ["$open", "$close", "$high", "$low", "$volume", "$factor"]
-data = (
-    (
-        D.features(instruments, fields, start_time="2018-01-01", end_time="2019-12-31", freq="day")
-        .swaplevel()
-        .sort_index()
-    )
+all_data = (
+    D.features(instruments, fields, freq="day")
     .swaplevel()
-    .loc[data.reset_index()["instrument"].unique()[:100]]
+    .sort_index()
+    .loc["2008-12-29":]
+    .sort_index()
+)
+
+all_data.to_hdf("./daily_pv_all.h5", key="data")
+
+
+debug_data = (
+    D.features(
+        instruments,
+        fields,
+        start_time="2018-01-01",
+        end_time="2019-12-31",
+        freq="day",
+    )
     .swaplevel()
     .sort_index()
 )
 
-data.to_hdf("./daily_pv_debug.h5", key="data")
+# 关键修复：从 debug_data 自己里面选存在于 2018-2019 的股票
+debug_instruments = debug_data.reset_index()["instrument"].drop_duplicates().to_numpy()[:100]
+
+debug_data = (
+    debug_data
+    .swaplevel()
+    .loc[debug_instruments]
+    .swaplevel()
+    .sort_index()
+)
+
+debug_data.to_hdf("./daily_pv_debug.h5", key="data")
